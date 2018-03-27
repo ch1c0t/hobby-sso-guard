9e6e9df48391e8c6a4855d05d8451d4d42f10746
Anatoly Chernow <chertoly@gmail.com>
2018-03-27 21:58:15 +0300

    A bit more acceptable specs

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,14 +1,24 @@
 require_relative 'helper'
 
 describe do
-  it do
-    browser = Watir::Browser.new
-    browser.goto TEST_APP_URL
-    sleep 1
-    assert { browser.text == '8081' }
+  let(:browser) { Watir::Browser.new }
+
+  context 'when there is no active session' do
+    it 'redirects to the auth server' do
+      browser.goto TEST_APP_URL
+      assert { browser.text == AUTH_SERVER_PORT.to_s }
+    end
+  end
+
+  context 'when a user tries to /enter' do
+    it 'refuses if there is no ticket' do
+      browser.goto "#{TEST_APP_URL}/enter"
+      assert { browser.text == 'No ticket.' }
+    end
   end
 
   after do
+    browser.quit
     $sessions.flushall
     $tickets.flushall
     $tokens.flushall
