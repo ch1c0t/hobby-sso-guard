423875661fa233d61d9eaa98636007c2cf744490
Anatoly Chernow <chertoly@gmail.com>
2018-03-28 00:34:52 +0300

    A few more specs

--- spec/auth_server.rb
+++ spec/auth_server.rb
@@ -5,7 +5,20 @@ class AuthServer
   include Hobby
 
   get do
-    ap env
-    env['SERVER_PORT']
+    if $auth_server_just_returns_HTTP_HOST
+      ap env
+      env['HTTP_HOST']
+    else
+      session, ticket = random, random
+      $sessions.set session, 'some_user_id'
+      $tickets.set ticket, session
+      
+      response.status = 302
+      response.redirect "#{TEST_APP_URL}/enter?ticket=#{ticket}"
+    end
+  end
+
+  def random
+    SecureRandom.urlsafe_base64 64
   end
 end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -5,8 +5,13 @@ describe do
 
   context 'when there is no active session' do
     it 'redirects to the auth server' do
+      $auth_server_just_returns_HTTP_HOST = true
       browser.goto TEST_APP_URL
-      assert { browser.text == AUTH_SERVER_PORT.to_s }
+      assert { browser.text == '127.0.0.1:8081' }
+    end
+
+    after do
+      $auth_server_just_returns_HTTP_HOST = false
     end
   end
 
@@ -14,6 +19,19 @@ describe do
     it 'refuses if there is no ticket' do
       browser.goto "#{TEST_APP_URL}/enter"
       assert { browser.text == 'No ticket.' }
+    end
+
+    it 'refuses if there is a bad ticket' do
+      bad_ticket = SecureRandom.urlsafe_base64 64
+      browser.goto "#{TEST_APP_URL}/enter?ticket=#{bad_ticket}"
+      assert { browser.text == 'Bad ticket.' }
+    end
+
+    context 'when the ticket is valid' do
+      it 'sets a session cookie and redirect to an appropriate path' do
+        browser.goto "#{TEST_APP_URL}/some_path"
+        assert { browser.text == 'some path in test app' }
+      end
     end
   end
 
